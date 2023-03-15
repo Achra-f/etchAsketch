745266c7e3738a053f76373b95b9881243bd10cb
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-03-15 21:01:34 +0100

    implement grid with javascript

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-let reset = document.querySelector('#reset');
+// select container
+const container = document.getElementById('container');
 
-const grid = document.querySelector(".grid");
+// create grid
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
-let input = document.getElementById('input');
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement('div');
+        cell.textContent = i +1;
+        container.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeRows(16, 16);
+
+
+
