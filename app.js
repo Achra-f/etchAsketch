7693026e3122a6b83c8569a30c13c2d7934cf3c1
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-03-17 17:14:52 +0100

    fixed prompt

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const button = document.getElementById('button');
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-
   for (let i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div');
     cell.textContent = i + 1;
@@ -24,15 +23,11 @@ grid.addEventListener("mouseover", function (event) {
   event.target.classList.replace("grid", "color");
 });
 
-function askGrid(){
-  let n = prompt("How many squares?: ")
-  return n
-}
 
-let x = askGrid();
+let userInput = prompt('number: ');
 
+makeRows(userInput, userInput);
 
-makeRows(x, x);
 button.addEventListener('click', () => askGrid());
 
 
