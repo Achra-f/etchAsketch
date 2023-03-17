759df7ac0242f3401b07689d422d1d260ed4385c
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-03-17 02:38:05 +0100

    hover effect + user input

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 // select container
 const container = document.getElementById('container');
+const button = document.getElementById('button');
 
 // create grid
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
 
-    for (let i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement('div');
-        cell.textContent = i +1;
-        container.appendChild(cell).className = 'grid-item';
-    };
+  for (let i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement('div');
+    cell.textContent = i + 1;
+    container.appendChild(cell).className = 'grid';
+  };
 };
 
-makeRows(16, 16);
+
+
+
+
+
+const grid = document.querySelector('div')
+grid.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("grid", "color");
+});
+
+function askGrid(){
+  let n = prompt("How many squares?: ")
+  return n
+}
+
+let x = askGrid();
+
+
+makeRows(x, x);
+button.addEventListener('click', () => askGrid());
 
 
 
