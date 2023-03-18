6b09910ed6dea03417e8562dcc8dc63b7f69b853
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-03-18 01:11:28 +0100

    user input + correct styling

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 // select container
 const container = document.getElementById('container');
-const button = document.getElementById('button');
-
 // create grid
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div');
-    cell.textContent = i + 1;
+    cell.textContent = "";
     container.appendChild(cell).className = 'grid';
   };
 };
 
 
-
-
-
-
-const grid = document.querySelector('div')
+//select grid
+const grid = document.querySelector('div');
+//mouseover grid
 grid.addEventListener("mouseover", function (event) {
   event.target.classList.replace("grid", "color");
 });
 
 
-let userInput = prompt('number: ');
 
-makeRows(userInput, userInput);
+//button grid
+const number = document.getElementById('number');
+const button = document.getElementById('button');
 
-button.addEventListener('click', () => askGrid());
+button.addEventListener("click", function () {
+
+  let userInput = number.value;
+  makeRows(userInput, userInput);
+
+}, false);
 
 
 
